224c595741f48f384d87a4859b2717cac61e162e
Raashidh <raashidh24@gmail.com>
2025-06-05 06:38:42 +0530

    Shape Created

--- src/Shape.java
+++ src/Shape.java
@@ -1,2 +1,23 @@
 public class Shape {
+    private String shapeName;
+
+    public Shape(){
+        shapeName = "N/A";
+
+    }
+    public Shape(String shapeName){
+        this.shapeName = shapeName;
+    }
+    public void displayShapeDetails(){
+        System.out.println("Shape Name: " + shapeName);
+
+    }
+    public double calculateArea(){
+        return 0;
+    }
+    public double calculateArea(double factor){
+        return calculateArea() * factor;
+    };
+
+
 }
