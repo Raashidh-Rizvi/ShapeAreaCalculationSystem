7f5f921820f2f1ad1d59b04730069edee55221d2
Raashidh <raashidh24@gmail.com>
2025-06-05 07:03:04 +0530

    Rectangle Created

---
+++ src/Rectangle.java
@@ -0,0 +1,29 @@
+public class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(){
+        super();
+        this.width = 0;
+        this.height = 0;
+    }
+    public Rectangle(String shapeName,double width, double lenght){
+        super(shapeName);
+        this.width = width;
+        this.height = lenght;
+    }
+
+
+    @Override
+    public double calculateArea(){
+        return  width*height;
+    }
+
+    public void diplayShapeDetails(){
+        super.displayShapeDetails();
+        System.out.println("Height:"+height);
+        System.out.println("Width:"+width);
+        System.out.println("Area: " + calculateArea());
+
+    }
+}
